1b138088d63ae3f75da2b19712985deea273a480
Giovanni Armane <giovanniarmane@gmail.com>
2022-10-11 21:42:49 -0300

    feat(Player): Basic player movement

---
+++ Player/Player.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+func _physics_process(delta):
+	var input_vector = Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	)
+	move_and_collide(input_vector)
